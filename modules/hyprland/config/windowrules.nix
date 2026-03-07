98b26a1c7be10ef5513a4e94550ff58aa3cd45cd
cedev <cedevserver@gmail.com>
2026-03-07 22:08:00 +0100

    fix: window rules PiP

--- modules/hyprland/config/windowrules.nix
+++ modules/hyprland/config/windowrules.nix
@@ -3,9 +3,9 @@
 ### WINDOW RULES ###
 ####################
 
-# Picture-in-Picture
-windowrulev2 = float, title:(Picture-in-Picture)
-windowrulev2 = move 50%-200 100%-250, title:(Picture-in-Picture)
-windowrulev2 = size 400 225, title:(Picture-in-Picture)
-windowrulev2 = pin, title:(Picture-in-Picture)
+# Picture-in-Picture (Firefox)
+windowrulev2 = float, title:Picture-in-Picture
+windowrulev2 = size 400 225, title:Picture-in-Picture  
+windowrulev2 = move 712 855, title:Picture-in-Picture
+windowrulev2 = pin, title:Picture-in-Picture
 ''
